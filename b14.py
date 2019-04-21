89ec0ad76f3a179966b4f4e0b21773a9d60c42d9
madhumithaasaravanan <49307435+madhumithaasaravanan@users.noreply.github.com>
2019-04-21 10:32:56 +0530

    b14.py

---
+++ b14.py
@@ -0,0 +1,7 @@
+starting=int(input("Enter the starting interval:"))
+end=int(input("Enter the starting interval:"))
+
+for num in range(starting,end + 1):
+
+    if num % 2 !=0:
+        print(num, end = " ")
